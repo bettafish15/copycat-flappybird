0170b4b9b8d56612285ba266f33698b613cf7056
nguyen hoang hung <bettafish6@gmail.com>
2021-02-04 12:08:05 +0700

    production ready

--- App.js
+++ App.js
@@ -1,21 +1,210 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import * as Font from "expo-font";
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  Alert,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import Bird from "./Bird";
+import Floor from "./Floor";
+import Constants from "./Constants";
+import { GameEngine } from "react-native-game-engine";
+import Matter from "matter-js";
+import Physics, {resetPipes} from "./Physics";
+import Images from './assets/Images';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.gameEngine = null;
+    this.entities = this.setupWorld();
+
+    this.state = {
+      running: true,
+      score: 0,
+    };
+  }
+
+  _loadFontsAsync = async () => {
+    // loadAsync returns true | error
+    let isLoaded = await Font.loadAsync({
+      // add as many fonts as you want here ....
+      FB: require("./assets/fonts/FB.ttf"),
+    });
+    this.setState({ loaded: isLoaded });
+  };
+
+  // call _loadFontsAsync
+  componentDidMount() {
+    this._loadFontsAsync();
+  }
+
+  setupWorld = () => {
+    let engine = Matter.Engine.create({ enableSleeping: false });
+    let world = engine.world;
+    world.gravity.y = 0.0;
+
+    let bird = Matter.Bodies.rectangle(
+      Constants.MAX_WIDTH / 2,
+      Constants.MAX_HEIGHT / 2,
+      Constants.BIRD_WIDTH,
+      Constants.BIRD_HEIGHT
+    );
+
+    let floor1 = Matter.Bodies.rectangle(
+      Constants.MAX_WIDTH / 2,
+      Constants.MAX_HEIGHT - 25,
+      Constants.MAX_WIDTH + 4,
+      50,
+      { isStatic: true }
+    );
+
+    let floor2 = Matter.Bodies.rectangle(
+      Constants.MAX_WIDTH + (Constants.MAX_WIDTH / 2),
+      Constants.MAX_HEIGHT - 25,
+      Constants.MAX_WIDTH + 4,
+      50,
+      { isStatic: true }
+    );
+   
+
+    Matter.World.add(world, [bird, floor1, floor2]);
+
+    Matter.Events.on(engine, "collisionStart", (event) => {
+      let pairs = event.pairs;
+
+      this.gameEngine.dispatch({ type: "game-over" });
+    });
+
+    return {
+      physics: { engine: engine, world: world },
+      bird: { body: bird, pose: 1, renderer: Bird },
+      floor1: {
+        body: floor1,
+        renderer: Floor,
+      },
+      floor2: {
+        body: floor2,
+        renderer: Floor,
+      },
+    };
+  };
+
+  onEvent = (e) => {
+    if (e.type === "game-over") {
+      this.setState({
+        running: false,
+      });
+    }else if(e.type === "score"){
+      this.setState({
+        score: this.state.score+1
+      })
+    }
+  };
+
+  reset = () => {
+    resetPipes();
+    this.gameEngine.swap(this.setupWorld());
+    this.setState({
+      running: true,
+      score: 0
+    });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image source= {Images.background} style={styles.backgroundImage} resizeMode="stretch"/>
+        <GameEngine
+          ref={(ref) => {
+            this.gameEngine = ref;
+          }}
+          style={styles.gameContainer}
+          systems={[Physics]}
+          running={this.state.running}
+          onEvent={this.onEvent}
+          entities={this.entities}
+        />
+        <Text style={styles.score}>{this.state.score}</Text>
+        {!this.state.running && (
+          <TouchableOpacity
+            onPress={this.reset}
+            style={styles.fullScreenButton}
+          >
+            <View style={styles.fullScreen}>
+              <Text style={styles.gameOverText}>Game Over</Text>
+              <Text style={styles.gameOverSubText}>Try Again</Text>
+            </View>
+          </TouchableOpacity>
+        )}
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ffffff",
   },
+  backgroundImage: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    width: Constants.MAX_WIDTH,
+    height: Constants.MAX_HEIGHT,
+  },
+  gameContainer: {
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+  fullScreenButton: {
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    flex: 1,
+  },
+  fullScreen: {
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: "black",
+    opacity: 0.8,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  gameOverText: {
+    color: "white",
+    fontSize: 40,
+    fontFamily: 'FB',
+  },
+  gameOverSubText: {
+    color: "white",
+    fontSize: 24,
+    fontFamily: 'FB',
+  },
+  score: {
+    position: 'absolute',
+    color: 'white',
+    fontFamily: 'FB',
+    fontSize: 72,
+    top: 50,
+    left: Constants.MAX_WIDTH/2 - 20,
+    textShadowColor: '#444444',
+    textShadowOffset: {width: 2, height: 2},
+    textShadowRadius: 2
+  }
 });
---
+++ Constants.js
@@ -0,0 +1,12 @@
+import {Dimensions} from 'react-native';
+
+var Constants = {
+    MAX_WIDTH: Dimensions.get('screen').width,
+    MAX_HEIGHT: Dimensions.get('screen').height,
+    GAP_SIZE: 400,
+    PIPE_WIDTH: 100,
+    BIRD_WIDTH: 50,
+    BIRD_HEIGHT: 41,
+}
+
+export default Constants;
